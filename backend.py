0d3feb3bf6fc4636884996b9a34f3e44a8ddcd52
Samarendra M Hedaoo <knightsamar@gmail.com>
2014-07-17 10:04:12 +0530

    Outgoing messages are now enqueued in the database for delivery.

--- backend.py
+++ backend.py
@@ -1,16 +1,43 @@
 from rapidsms.backends.base import BackendBase
+import logging
+import requests
+from .models import EnqueuedMessage
+
+logger = logging.getLogger('envayasms.backend.EnvayaSMSBackend')
 
 class EnvayaSMSBackend(BackendBase):
     '''This handles the outgoing part ONLY'''
 
-    '''def configure(**kwargs):
-        print kwargs
-        pass;
-    '''
+    def configure(self, **kwargs):
+        '''Handles specific configuration for this Backend'''
+        super(EnvayaSMSBackend, self).configure(**kwargs)
 
-    def send(id_, text, identities, context = {}):
-        print id
-        print text
-        print identities
-        print context
-        pass;
+    def send(self, id_, text, identities, context = {}):
+        '''
+        This handles the actual part of outgoing message cycle.
+        What we actually do here, is enqueue the message to be sent when the Android app
+        enquires about it.
+        '''
+        logger.debug("id is %s" % id_)
+        logger.debug("text is %s" % text)
+        logger.debug("identities is %s" % type(identities))
+        logger.debug("context is %s " % context)
+
+        for i in identities:
+            m = EnqueuedMessage(
+                msg_id   = id_ ,
+                recipient= i,
+                message  = text,
+                )
+            m.save()
+
+    @property
+    def model(self):
+        """
+        The model attribute is the RapidSMS model instance with this
+        backend name. A new backend will automatically be created if
+        one doesn't exist upon accessing this attribute.
+        """
+        from rapidsms.models import Backend
+        backend, _ = Backend.objects.get_or_create(name=self.name)
+        return backend
--- models.py
+++ models.py
@@ -3,9 +3,16 @@ from django.db.models import Q
 import datetime
 
 class EnqueuedMessage(models.Model):
+    STATUS_CHOICES = (
+            ('q', 'Queued for delivery'),
+            ('s', 'Successfully sent'),
+            ('x', 'Error while sending'),
+            )
+    msg_id  = models.CharField(max_length=32, null=False, blank=False, primary_key=True)
     sent_at = models.DateField(auto_now=True, db_index=True)
     message = models.TextField()
     recipient = models.CharField(db_index=True, max_length=255) # not using contacts so we can query the field
+    status = models.CharField(choices=STATUS_CHOICES, max_length=1, default='q')
 
     @classmethod
     def messages_for(cls, country_code, max_delay):
